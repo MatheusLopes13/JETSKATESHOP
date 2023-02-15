9a6ab80c8fefa2a19b941c32a2cd75a1bc522179
matheus lopes <matheuuslopes13@gmail.com>
2023-02-15 01:59:20 -0300

    home correções

---
+++ src/produto/produto.js
@@ -0,0 +1,21 @@
+const produto = [
+    {
+        imagem: "https://png.pngtree.com/png-vector/20210320/ourmid/pngtree-t-shirt-summer-photography-with-black-clothes-png-image_3087107.jpg",
+        nome: "camisa",
+        cor: "preta",
+        tamanhos: ["p", "g", "gg"],
+    },
+    {
+        imagem: "https://loja.comerciomix.com.br/media/catalog/product/cache/fb4f878514d02efd710032ded901d118/b/r/branca1_2.png",
+        nome: "camisa",
+        cor: "branca",
+        tamanhos: ["p", "m", "g"]
+        
+    },
+    {
+        imagem: "https://e7.pngegg.com/pngimages/7/584/png-clipart-t-shirt-polo-shirt-ralph-lauren-corporation-clothing-t-shirt-tshirt-orange.png",
+        nome: "camisa",
+        cor: "laranja",
+        tamanhos: ["pp", "m", "g"]
+    }
+]
