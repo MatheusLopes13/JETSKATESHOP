8d7c0eb7b88cd1e40c6aeb091d66d64e6bb928e8
gooncalvesgaby <gabrielle.gonalves@outlook.com.br>
2023-02-22 22:45:54 -0300

    alteração no botao de finalizar compra usando js, linkei pagamento.js com pagamento.html

---
+++ src/login/login.js
@@ -0,0 +1,12 @@
+/* Passo um: Buscar o email digitado.
+Passo dois: Buscar a senha digitada.
+Passo três: Se email e senha forem válidos eu mando ele pra pagina home. 
+Passo quatro: Se email ou senha forem inválidos, eu mando o comando usuario invalido.
+*/
+
+
+function dizerOi(){
+    return "OI"
+}
+console.log(dizerOi())
+
