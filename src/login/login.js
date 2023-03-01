763982a258c6040bf4b95a49e418226af83720ed
gooncalvesgaby <gabrielle.gonalves@outlook.com.br>
2023-03-01 16:40:55 -0300

    implementei funcionalidades na pag login usando eventos, coloquei algumas ações usando JS

--- src/login/login.js
+++ src/login/login.js
@@ -1,2 +1,27 @@
 
 
+function entrar(){
+    const email = document.getElementById('email');
+    const senha = document.getElementById('senha');
+
+    if(email.value === ""){
+        let mensagem = document.getElementById('email-aviso')
+        adicionarClasseErro(email, mensagem);
+    }
+  
+    if(senha.value === '') {
+        let mensagem = document.getElementById('senha-aviso')
+        adicionarClasseErro(senha,mensagem)
+       
+    } 
+    
+}
+
+function adicionarClasseErro(elemento, idMensagem){
+    idMensagem.style.display = "inline"
+    elemento.style.borderColor = "red"
+}
+
+
+
+
