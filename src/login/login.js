13a616ea518c8869762ff103514e402411805195
gooncalvesgaby <gabrielle.gonalves@outlook.com.br>
2023-03-08 00:51:36 -0300

    validações inputs tela pagamento, alterações em alertas

--- src/login/login.js
+++ src/login/login.js
@@ -1,5 +1,4 @@
-// validar se a senha e o validar senha sao iguais;
-// validar os erros;
+
 
 
 function entrar(){
@@ -8,15 +7,15 @@ function entrar(){
     
 
     if(email.value === "" || validarEmail(email.value) === false){
-        let mensagem = document.getElementById('email-aviso')
+        let mensagem = document.getElementById('email-aviso');
         adicionarClasseErro(email, mensagem);
-        alert('Por favor, preencha o seu email!')
+        
     }
   
     if(senha.value === '' || senha.value.length < 8) {
-        let mensagem = document.getElementById('senha-aviso')
-        adicionarClasseErro(senha,mensagem)
-        alert('Informe uma senha com no mínimo 8 caracteres');
+        let mensagem = document.getElementById('senha-aviso');
+        adicionarClasseErro(senha,mensagem);
+        
     } 
 
     
@@ -25,48 +24,48 @@ function entrar(){
 
 
 function adicionarClasseErro(elemento, idMensagem){
-    idMensagem.style.display = "inline"
-    elemento.style.borderColor = "red"
+    idMensagem.style.display = "inline";
+    elemento.style.borderColor = "red";
 }
 
 function cadastrar(){
-    const nome = document.getElementById('nome')
-    const sobrenome = document.getElementById('sobrenome')
-    const logar = document.getElementById('logar')
-    const entrar = document.getElementById('entrar')
-    const confirma = document.getElementById('confirma')
+    const nome = document.getElementById('nome');
+    const sobrenome = document.getElementById('sobrenome');
+    const logar = document.getElementById('logar');
+    const entrar = document.getElementById('entrar');
+    const confirma = document.getElementById('confirma');
     
 
     if(nome.value === '' || validarNome(nome.value) === false){
-        let mensagem = document.getElementById('nome-aviso')
-        adicionarClasseErro(nome, mensagem)
+        let mensagem = document.getElementById('nome-aviso');
+        adicionarClasseErro(nome, mensagem);
 
     }
 
     if(sobrenome.value === '' || validarNome(sobrenome.value) === false){
-        let mensagem = document.getElementById('sobrenome-aviso')
-        adicionarClasseErro(sobrenome, mensagem)
+        let mensagem = document.getElementById('sobrenome-aviso');
+        adicionarClasseErro(sobrenome, mensagem);
 
     }
 
     if(logar.value === '' || validarEmail(logar.value) === false){
-        let mensagem = document.getElementById('logar-aviso')
-        adicionarClasseErro(logar, mensagem)
-        alert("Por favor, preencha seu email!")
+        let mensagem = document.getElementById('logar-aviso');
+        adicionarClasseErro(logar, mensagem);
+        
 
     }
 
     if(entrar.value === '' || entrar.value.length < 8){
-        let mensagem = document.getElementById('entrar-aviso')
-        adicionarClasseErro(entrar, mensagem)
-        alert('Informe uma senha com no mínimo 8 caracteres');
+        let mensagem = document.getElementById('entrar-aviso');
+        adicionarClasseErro(entrar, mensagem);
+       
 
     }
 
     if(confirma.value === '' || confirma.value.length < 8 || validarSenha(entrar.value, confirma.value) === false){
-        let mensagem = document.getElementById('confirma-aviso')
-        adicionarClasseErro(confirma, mensagem)
-        alert('Senhas não são iguais.');
+        let mensagem = document.getElementById('confirma-aviso');
+        adicionarClasseErro(confirma, mensagem);
+        
     }
 
         
--- src/pagamento/pagamento.js
+++ src/pagamento/pagamento.js
@@ -1,5 +1,61 @@
-const button = document.querySelectorAll('button')
-button.forEach(element => {
-    element.style.background = '#aca6a6'
-})
+function pagar(){
+    const cartao = document.getElementById('cartao');
+    const validade = document.getElementById('validade');
+    const codigoSeguranca = document.getElementById('seguranca');
+    const cep = document.getElementById('cep');
+    const endereco = document.getElementById('endereco');
+    const numero = document.getElementById('numero');
+    const referencia = document.getElementById('referencia');
+
+   if(cartao.value === '' || cartao.value.length < 16){
+    const mensagem = document.getElementById('aviso-pagamento');
+    mensagem.style.display = 'inline';
+
+   }
+    
+   if(validade.value === '' || validade.value.length < 5){
+    const mensagem = document.getElementById('aviso-validade');
+    mensagem.style.display = 'inline';
+   }
+
+   if(codigoSeguranca.value === '' || codigoSeguranca.value.length < 3){
+    const mensagem = document.getElementById('aviso-seguranca');
+    mensagem.style.display = 'inline';
+
+   }
+ 
+   if(cep.value === '' || cep.value.length < 8){
+       const mensagem = document.getElementById('aviso-cep');
+       mensagem.style.display = 'inline';
+
+   }
+
+   if(endereco.value === ''){
+       const mensagem = document.getElementById('aviso-endereco');
+       mensagem.style.display = 'inline';
+
+   }
+
+   if(numero.value === ''){
+    const mensagem = document.getElementById('aviso-numero');
+    mensagem.style.display = 'inline';
+
+    }
+
+    if(referencia.value === ''){
+        const mensagem = document.getElementById('aviso-referencia');
+        mensagem.style.display = 'inline';
+ 
+    }
+
+   
+
+
+
+}
+
+
+
+
+
 
